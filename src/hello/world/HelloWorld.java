7887de0f1b8b46afbd78fb4f738aea513776111c
avh1 <avh1@FSTU603018.pti.edu>
2019-02-01 09:14:25 -0500

    Added Hale() method for name print out control
    Added ability for main method to call Hale() method
    
    SECOND COPY

---
+++ src/hello/world/HelloWorld.java
@@ -0,0 +1,16 @@
+package hello.world;
+
+public class HelloWorld 
+{
+
+    public static void main(String[] args) 
+    {
+        Hale();
+    }
+    
+    public static void Hale()
+    {
+        System.out.println("Austin Hale!!");
+    }
+    
+}
